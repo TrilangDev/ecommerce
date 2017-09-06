cfe5abcd14245e0a8ffcef11a22b4bb016856a02
TrilangDev <teste@trilang.com>
2017-09-06 12:05:36 -0300

    Configurado Tpl

---
+++ views-cache/header.003873c670aac278cd0a8e0d7f83d824.rtpl.php
@@ -0,0 +1,11 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <title>Hcode</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
